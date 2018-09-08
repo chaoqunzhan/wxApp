7e6555bfee62a6117f890757e577ed4977a86ba1
unknown <you@example.com>
2018-09-08 22:51:58 +0800

    wx

---
+++ pages/teacher/teacher.js
@@ -0,0 +1,17 @@
+//teacher.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    indicatorDots: true,  //显示面板指示点
+    dotsColor: '#aaa',
+    autoplay: true,     //自动切换
+    interval: 5000,    //自动切换时间间隔
+    duration: 1000,    //滑动动画时长
+    imgUrls: [
+      '../../images/about/bonner/bonner1.jpg',   // 本地读取图片的方式
+      '../../images/about/bonner/bonner2.jpg',
+      '../../images/about/bonner/bonner3.jpg',
+    ],
+  }
+})
